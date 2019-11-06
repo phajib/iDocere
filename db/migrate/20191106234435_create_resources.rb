b1f4e3a3d9107bb73774f721b71f059ecef48585
Haj B <phajib@gmail.com>
2019-11-06 15:49:25 -0800

    generate resources

---
+++ db/migrate/20191106234435_create_resources.rb
@@ -0,0 +1,14 @@
+class CreateResources < ActiveRecord::Migration[6.0]
+  def change
+    create_table :resources do |t|
+      t.integer :grade_level
+      t.string, :subject
+      t.string :assignment
+      t.integer :teacher_id
+      t.integer :parent_id
+      t.integer :student_id
+
+      t.timestamps
+    end
+  end
+end
