cf514651591b6a8c2437456d78c481bcb14c211b
Haj B <phajib@gmail.com>
2019-11-07 01:50:42 -0800

    devise Teacher

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :teachers
+  root 'resources#index'
+  
   resources :messages
   resources :resources
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
