9afb7e098637b809b48bc403400603403355a799
Haj B <phajib@gmail.com>
2019-11-06 16:17:51 -0800

    subject string

--- db/migrate/20191106234435_create_resources.rb
+++ db/migrate/20191106234435_create_resources.rb
@@ -2,7 +2,7 @@ class CreateResources < ActiveRecord::Migration[6.0]
   def change
     create_table :resources do |t|
       t.integer :grade_level
-      t.string, :subject
+      t.string :subject
       t.string :assignment
       t.integer :teacher_id
       t.integer :parent_id
