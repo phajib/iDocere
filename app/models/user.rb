d444990719ff5bda974167f0eae781495ab3615c
Haj B <phajib@gmail.com>
2019-11-14 00:51:39 -0800

    User model set

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable
+  
+  has_many :resources, dependent: :destroy
+  has_many :messages
+  has_many :comments, dependent: :destroy
+  has_many :parents
+  has_many :students, through: :parents
 end
