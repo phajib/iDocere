7968fc902d3ab679871f9d6b191e3177827cfd09
Haj B <phajib@gmail.com>
2019-11-17 17:00:19 -0800

    user

--- db/migrate/20191106234435_create_resources.rb
+++ db/migrate/20191106234435_create_resources.rb
@@ -4,7 +4,7 @@ class CreateResources < ActiveRecord::Migration[6.0]
       t.integer :grade_level
       t.string :subject
       t.string :assignment
-      t.integer :teacher_id
+      t.integer :user_id
       t.integer :parent_id
       t.integer :student_id
       t.integer :message_id
--- db/migrate/20191107001124_create_messages.rb
+++ db/migrate/20191107001124_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[6.0]
     create_table :messages do |t|
       t.string :title
       t.text :description
-      t.integer :teacher_id
+      t.integer :user_id
       t.integer :parent_id
       t.string :resource_id
 
