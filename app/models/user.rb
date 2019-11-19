69e1688a7aff383b720c8ce08ade24a385a59018
Haj B <phajib@gmail.com>
2019-11-19 14:25:41 -0800

    clean up comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,8 +32,6 @@ class User < ApplicationRecord
   end
 
 
-  # new function to set the password without knowing the current
-  # password used in our confirmation controller.
   def attempt_set_password(params)
     p = {}
     p[:password] = params[:password]
@@ -48,19 +46,15 @@ class User < ApplicationRecord
      password == password_confirmation && !password.blank?
   end
 
-  # new function to return whether a password has been set
   def has_no_password?
     self.encrypted_password.blank?
   end
 
-  # Devise::Models:unless_confirmed` method doesn't exist in Devise 2.0.0 anymore.
-  # Instead you should use `pending_any_confirmation`.
   def only_if_unconfirmed
     pending_any_confirmation {yield}
   end
 
   def password_required?
-  # Password is required if it is being set, but not for new records
     if !persisted?
       false
     else
